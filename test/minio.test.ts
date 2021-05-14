318106f9bf22fd97123a820d7d5ba6435418b1da
AndersXiao <sharping@outlook.com>
2021-05-14 15:28:22 +0800

    radom unit test

--- test/minio.test.ts
+++ test/minio.test.ts
@@ -1,3 +1,4 @@
+import { generateUUID } from "../src/utils";
 import { BucketPolicy } from "../src/core";
 import { MinioUtils } from "../src/minio";
 import { initRequestNoneOAuth2 } from "../src/request";
@@ -16,8 +17,10 @@ test("testAssumeRoleUpload", async ()=>{
     const resp = await minioUtils.fetchConfig(false, true);
     expect(resp.response.ok).toBeTruthy();
 
-    const uploadResp = await minioUtils.upload(BucketPolicy.Public, "tttt.txt", "XXXXXX");
+    const file = generateUUID()
+    const uploadResp = await minioUtils.upload(BucketPolicy.Public, `aaa/${file}.txt`, "XXXXXX");
     const { response, data } = uploadResp;
     expect(response.ok).toBeTruthy();
     expect(response.status).toEqual(200);
+    expect(data.etag.length >0).toBeTruthy();
  });
